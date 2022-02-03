87f463fc0a22190be06f5d1e0880f96499a2a1e1
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-02-03 12:51:16 +0530

    Update antiflood.py

--- Flare_Robot/modules/antiflood.py
+++ Flare_Robot/modules/antiflood.py
@@ -85,7 +85,7 @@ def check_flood(update, context) -> str:
             execstrings = "Muted for {}".format(getvalue)
             tag = "TMUTE"
         send_message(
-            update.effective_message, "Beep Boop! Boop Beep!\n{}!".format(execstrings),
+            update.effective_message, "This is not the place to spam, you Noob!\n{}!".format(execstrings),
         )
 
         return (
@@ -404,13 +404,13 @@ __help__ = """
 Antiflood allows you to take action on users that send more than x messages in a row. Exceeding the set flood \
 will result in restricting that user.
  This will mute users if they send more than 10 messages in a row, bots are ignored.
- • `/flood`*:* Get the current flood control setting
-• *Admins only:*
- • `/setflood <int/'no'/'off'>`*:* enables or disables flood control
+ ✧ `/flood`*:* Get the current flood control setting
+➲ *Admins only:*
+ ✧ `/setflood <int/'no'/'off'>`*:* enables or disables flood control
  *Example:* `/setflood 10`
- • `/setfloodmode <ban/kick/mute/tban/tmute> <value>`*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
-• *Note:*
- • Value must be filled for tban and tmute!!
+ ✧ `/setfloodmode <ban/kick/mute/tban/tmute> <value>`*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
+➲ *Note:*
+ ✧ Value must be filled for tban and tmute!!
  It can be:
  `5m` = 5 minutes
  `6h` = 6 hours
