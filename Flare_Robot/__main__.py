d133e26fabbd9f3a4037aecb7b3148561957522a
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-02-10 08:37:42 +0530

    Update __main__.py

--- Flare_Robot/__main__.py
+++ Flare_Robot/__main__.py
@@ -241,7 +241,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_animation(
-            GROUP_START_IMG, caption= "I won't sleep because I believe someone is waiting for your music. I'm awake since :</b> <code>{}</code>".format(
+            GROUP_START_IMG, caption= "I won\'t sleep because I believe someone is waiting for your music.\n I\'m awake since :</b> <code>{}</code>".format(
                 uptime
             ),
             parse_mode=ParseMode.HTML,
